ccb7251a4fe7a23b02d6d04f6b4871b471c6c633
Nahom Berhane <nahom.berhane@nav.no>
2025-06-17 09:27:39 +0200

    setup frontend for brum

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@navikt/ds-react";
+import { PageBlock } from "@navikt/ds-react/Page";
+import styles from "./page.module.css";
+import { ThumbUpIcon } from "@navikt/aksel-icons";
+
+export default function Home() {
+  return (
+    <main>
+      <PageBlock width="md" gutters>
+        <Button
+          icon={<ThumbUpIcon title="a11y tittel" />}
+          className={styles.limeButton}
+        >
+          Knapp
+        </Button>
+      </PageBlock>
+    </main>
+  );
+}
