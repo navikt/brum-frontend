947d04a429c0e7d45bbf52786f7ce5a8ecf47a53
Ingvild Utvik <ingvild.utvik@nav.no>
2025-06-17 16:40:45 +0200

    Set up dummy hardcoded copy of first graph

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,66 @@
-'use client' // ??
+"use client"; // ??
 
-import { Button } from "@navikt/ds-react";
 import { PageBlock } from "@navikt/ds-react/Page";
 import styles from "./page.module.css";
-import {
-  Chart,
-  Title
-} from '@highcharts/react';
+import { Chart, PlotOptions, Title, XAxis, YAxis } from "@highcharts/react";
 
-import {
-  Area,
-  Line
-} from '@highcharts/react/series';
+import { Column } from "@highcharts/react/series";
 
-export default function Home() {
+import { useState } from "react";
+import { Label, Radio, RadioGroup } from "@navikt/ds-react";
+
+const tiltaksdata = {
+  grot: { vedtak: [0, 3, 2], opptak: [1, 6, 5], skippertak: [0, 5, 4] },
+  suppe: { vedtak: [3, 1, 1], opptak: [2, 2, 2], skippertak: [2, 0, 1] },
+  spag: { vedtak: [6, 6, 4], opptak: [4, 5, 4], skippertak: [2, 1, 4] },
+};
+
+const Home = () => {
+  const [avdeling, setAvdeling] = useState("grot");
+
   return (
     <main>
       <PageBlock width="md" gutters>
+        <RadioGroup
+          legend="Velg avdeling."
+          onChange={setAvdeling}
+          value={avdeling}
+        >
+          <Radio value="grot">Grøtavdelingen</Radio>
+          <Radio value="suppe">Suppeavdelingen</Radio>
+          <Radio value="spag">Spagettiavdelingen</Radio>
+        </RadioGroup>
+      </PageBlock>
+      <PageBlock width="md" gutters>
         <Chart>
-            <Title>Chart with multiple series types</Title>
-            <Area.Series data={[0, 1, 2, 3]} />
-            <Line.Series data={[2, 1, 3, 1]} />
+          <Title>Andel av tiltak</Title>
+          <XAxis categories={["Februar", "Mars", "April"]} />
+          <YAxis min={0} stackLabels={{ enabled: true }} />
+
+          <PlotOptions
+            series={{
+              stacking: "normal",
+              dataLabels: {
+                enabled: true,
+              },
+            }}
+          />
+          <Column.Series
+            data={tiltaksdata[avdeling as keyof typeof tiltaksdata].vedtak}
+            options={{ name: "Vedtakstiltak" }}
+          />
+          <Column.Series
+            data={tiltaksdata[avdeling as keyof typeof tiltaksdata].opptak}
+            options={{ name: "Opptakstiltak" }}
+          />
+          <Column.Series
+            data={tiltaksdata[avdeling as keyof typeof tiltaksdata].skippertak}
+            options={{ name: "Skippertakstiltak" }}
+          />
         </Chart>
       </PageBlock>
     </main>
   );
-}
+};
+
+export default Home;
