3d543f7a6420d3a8fa9ed66e1b8279a67861bea7
Ingvild Utvik <ingvild.utvik@nav.no>
2025-06-17 10:26:53 +0200

    set up test graph with highcharts

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,27 @@
+'use client' // ??
+
 import { Button } from "@navikt/ds-react";
 import { PageBlock } from "@navikt/ds-react/Page";
 import styles from "./page.module.css";
-import { ThumbUpIcon } from "@navikt/aksel-icons";
+import {
+  Chart,
+  Title
+} from '@highcharts/react';
+
+import {
+  Area,
+  Line
+} from '@highcharts/react/series';
 
 export default function Home() {
   return (
     <main>
       <PageBlock width="md" gutters>
-        <Button
-          icon={<ThumbUpIcon title="a11y tittel" />}
-          className={styles.limeButton}
-        >
-          Knapp
-        </Button>
+        <Chart>
+            <Title>Chart with multiple series types</Title>
+            <Area.Series data={[0, 1, 2, 3]} />
+            <Line.Series data={[2, 1, 3, 1]} />
+        </Chart>
       </PageBlock>
     </main>
   );
