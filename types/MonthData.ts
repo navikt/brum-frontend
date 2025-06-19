129c1e1f38664ec303bdf5cbbade05e0c28f1547
Ingvild Utvik <ingvild.utvik@nav.no>
2025-06-19 15:14:23 +0200

    Cleaned up unused files

--- types/MonthData.ts
+++
@@ -1,7 +0,0 @@
-export type MonthDataItem = {
-  month: string;
-  Avdeling: string;
-  AntallPaTiltak: string;
-  TiltaksType: string;
-  instasBehov: 'S.Tilpasset' | 'S.bestemt' | 'LandeGruppe3' | null;
-};
