12ea17d3096a50f9c30b7ae6c0748e9689f1cfb5
Ingvild Utvik <ingvild.utvik@nav.no>
2025-06-20 15:34:06 +0200

    fixed prop typing

--- app/graph.tsx
+++ app/graph.tsx
@@ -2,16 +2,18 @@
 
 import { Chart, Title } from '@highcharts/react';
 import { Data } from '@highcharts/react/options/Data';
-import { Series } from '@highcharts/react';
 
-const Graph = ({ filnavn }) => {
+interface PropsTypes {
+  filnavn: string;
+}
+
+const Graph = ({ filnavn }: PropsTypes) => {
   return (
     <div>
       <Chart>
         <Title>t</Title>
         <Data csvURL={'/data/' + filnavn} />
       </Chart>
-
     </div>
   );
 };
