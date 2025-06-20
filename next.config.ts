99c9c216ab08248a95ba37b1fb58943e75aba20d
Nahom Berhane <nahom.berhane@nav.no>
2025-06-20 14:54:55 +0200

    Removed/commented out CDN. Can be added later if needed

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  reactStrictMode: true,
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://cdn.nav.no/brum"
-      : undefined,
+  // reactStrictMode: true,
+  // assetPrefix:
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://cdn.nav.no/brum"
+  //     : undefined,
 };
 
 export default nextConfig;
