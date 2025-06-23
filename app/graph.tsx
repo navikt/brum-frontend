f71add5dbcfdf4ac4adf2a7054bacd1217dfc3cb
Ingvild Utvik <ingvild.utvik@nav.no>
2025-06-23 15:13:38 +0200

    Using chartoptions, got proper update working

--- app/graph.tsx
+++ app/graph.tsx
@@ -1,25 +1,38 @@
 'use client';
 
-import { Chart, PlotOptions, Series, Title } from '@highcharts/react';
+import { Chart, PlotOptions } from '@highcharts/react';
 import { Data } from '@highcharts/react/options/Data';
 import { Exporting } from '@highcharts/react/options/Exporting'; // tillater eksportering av grafen
-import { Column } from '@highcharts/react/series';
+import { Radio, RadioGroup } from '@navikt/ds-react';
+import { useRef, useState } from 'react';
 
-interface PropsTypes {
+interface GraphProps {
   filnavn: string;
 }
 
-const Graph = ({ filnavn }: PropsTypes) => {
+const Graph = ({ filnavn }: GraphProps) => {
+  const [inverted, setInverted] = useState(true);
+  const ref = useRef<any>(null);
+
+  const swapInversion = (e: boolean) => {
+    setInverted(e);
+    ref.current?.chart.update({ chart: { inverted: inverted } });
+  };
+
+  const chartOptions = { chart: { type: 'column', inverted: inverted } };
+
   return (
     <div>
-      <Chart>
+      <Chart options={chartOptions} ref={ref}>
         <Exporting />
         <Data csvURL={'/data/' + filnavn} />
         <PlotOptions series={{ stacking: 'normal' }} />
-        <Column.Series />
-        <Column.Series />
-        <Column.Series />
       </Chart>
+
+      <RadioGroup legend="Inverted?" onChange={swapInversion} value={inverted}>
+        <Radio value={true}>Yes</Radio>
+        <Radio value={false}>No</Radio>
+      </RadioGroup>
     </div>
   );
 };
