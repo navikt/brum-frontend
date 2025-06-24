64a2cee819c137efd78f2c5a8145166cdec54d26
Nahom Berhane <nahom.berhane@nav.no>
2025-06-24 10:02:47 +0200

    correct env var

--- app/env.ts
+++ app/env.ts
@@ -4,11 +4,13 @@ type ServerEnv = z.infer<typeof ServerEnvSchema>;
 
 const ServerEnvSchema = z.object({
     tokenExchangeEndpoint: z.string(),
+    backendAudience: z.string(),
 })
 
 export function getServerEnv(): ServerEnv {
     const env = ServerEnvSchema.parse({
-        tokenExchangeEndpoint: process.env.TOKEN_EXCHANGE_ENDPOINT
+        tokenExchangeEndpoint: process.env.NAIS_TOKEN_EXCHANGE_ENDPOINT,
+        backendAudience: process.env.BACKEND_AUDIENCE,
     })
 
     return env;
