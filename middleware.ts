591f6b5012c613007ffe9fa43cef5b1f5f1413c1
Nahom Berhane <nahom.berhane@nav.no>
2025-06-27 08:12:05 +0200

    Add logger integration and update package dependencies

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
 import { NextResponse, NextRequest } from 'next/server';
+import { logger } from '@navikt/next-logger';
 
 // oauth2 login path
 const NAIS_LOGIN_PATH = '/oauth2/login';
 
 export async function middleware(request: NextRequest) {
   console.log(`Middleware: Request received for ${request.nextUrl.pathname}`);
+  logger.warn(`Middleware: Request received for ${request.nextUrl.pathname}`);
   const { pathname, origin, href } = request.nextUrl;
 
   // Sjekk om path er offentlig eller login path
@@ -13,10 +15,12 @@ export async function middleware(request: NextRequest) {
 
   if (isPublicPath || isNaisLoginPath) {
     console.log(`Middleware: Public/Login path '${pathname}'. Proceeding.`);
+    logger.warn(`Middleware: Public/Login path '${pathname}'. Proceeding.`)
     return NextResponse.next();
   }
 
   try {
+    logger.warn(`Middleware: Checking session for protected path '${pathname}'.`);
     console.log(`Middleware: Checking session for protected path '${pathname}'.`);
 
     // Hent session data from the OAuth2 session endpoint
@@ -25,6 +29,7 @@ export async function middleware(request: NextRequest) {
 
     if (!sessionData.session?.active) {
       console.log(`Middleware: User not authenticated for ${pathname}. Redirecting to login.`);
+      logger.warn(`Middleware: User not authenticated for ${pathname}. Redirecting to login.`)
 
       // Hvis ikke sesion ikke er aktiv, omdiriger til login
       const loginUrl = new URL(NAIS_LOGIN_PATH, origin);
@@ -36,10 +41,12 @@ export async function middleware(request: NextRequest) {
 
     // Hvis session er aktiv, fortsett til den beskyttede siden
     console.log(`Middleware: User authenticated for ${pathname}. Proceeding.`);
+    logger.warn(`Middleware: User authenticated for ${pathname}. Proceeding.`);
     return NextResponse.next();
   } catch (error) {
     // Hvis det oppstår en feil under sjekk av session, logg feilen og omdiriger til login
     console.error(`Middleware: Error checking session for ${pathname}:`, error);
+    logger.warn(`Middleware: Error checking session for ${pathname}:`, error);
     const loginUrl = new URL(NAIS_LOGIN_PATH, origin);
     loginUrl.searchParams.set('redirect_uri', href);
     return NextResponse.redirect(loginUrl);
