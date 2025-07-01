b6611b92be7a724c3bc5a5f2e5cb02ba6bf580a4
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-01 09:36:26 +0200

    removed unused type

--- common/types/chartTypes.ts
+++ common/types/chartTypes.ts
@@ -1,13 +1,6 @@
 import { HighchartsOptionsType } from '@highcharts/react';
 import { Dispatch, SetStateAction } from 'react';
 
-export interface chartOptionType {
-  title: { text: string };
-  chart: { type: string; inverted: boolean };
-  series: any;
-  plotOptions: { series: { stacking: 'normal' | 'percent' | undefined } };
-}
-
 export interface MenuProps {
   chartOptions: HighchartsOptionsType;
   setChartOptions: Dispatch<SetStateAction<HighchartsOptionsType>>;
