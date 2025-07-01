6454bc452fb7f2bd41867b6ee25d877381e24092
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-01 16:06:44 +0200

    added temp version of percent mode

--- common/components/chartmenu.tsx
+++ common/components/chartmenu.tsx
@@ -1,47 +1,94 @@
 'use client';
-import { MenuProps } from '@/common/types/chartTypes';
-import { Radio, RadioGroup, TextField } from '@navikt/ds-react';
+import { ChartOptionsProps, PercentModeProps, StackingRadioProps } from '@/common/types/chartTypes';
+import { Radio, RadioGroup, Switch, TextField } from '@navikt/ds-react';
+import { useState } from 'react';
 
-const ChartMenu = ({ chartOptions, setChartOptions }: MenuProps) => {
+const ChartMenu = (props: ChartOptionsProps) => {
+  const [percentMode, setPercentMode] = useState(false);
+
   return (
     <div>
-      <RadioGroup
-        legend="Inverted?"
-        onChange={(e) => {
-          setChartOptions({
-            ...chartOptions,
-            chart: { type: chartOptions.chart!.type, inverted: e },
-          });
-        }}
-        value={chartOptions.chart!.inverted}
-      >
-        <Radio value={true}>Yes</Radio>
-        <Radio value={false}>No</Radio>
-      </RadioGroup>
-
-      <TextField
-        onChange={(e) => {
-          setChartOptions({ ...chartOptions, title: { text: e.target.value } });
-        }}
-        value={chartOptions.title!.text}
-        label="Sett tittel"
-      />
-
-      <RadioGroup
-        legend="Stacking?"
-        onChange={(e) => {
-          setChartOptions({
-            ...chartOptions,
-            plotOptions: { series: { stacking: e } },
-          });
-        }}
-        value={chartOptions.plotOptions!.series!.stacking}
-      >
-        <Radio value={'normal'}>Normal</Radio>
-        <Radio value={'percent'}>Percent</Radio>
-      </RadioGroup>
+      <TitleField {...props} />
+      <PercentModeSwitch {...props} percentMode={percentMode} setPercentMode={setPercentMode} />
+      <InversionRadio {...props} />
+      <StackingRadio {...props} percentMode={percentMode} />
     </div>
   );
 };
+
+const TitleField = ({ chartOptions, setChartOptions }: ChartOptionsProps) => (
+  <TextField
+    onChange={(e) => {
+      setChartOptions({ ...chartOptions, title: { text: e.target.value } });
+    }}
+    value={chartOptions.title!.text}
+    label="Sett tittel"
+  />
+);
+
+const InversionRadio = ({ chartOptions, setChartOptions }: ChartOptionsProps) => (
+  <RadioGroup
+    legend="Inverted?"
+    onChange={(e) => {
+      setChartOptions((prev) => ({
+        ...prev,
+        chart: { type: prev.chart!.type, inverted: e },
+      }));
+    }}
+    value={chartOptions.chart!.inverted}
+  >
+    <Radio value={true}>Yes</Radio>
+    <Radio value={false}>No</Radio>
+  </RadioGroup>
+);
+
+const StackingRadio = ({ chartOptions, setChartOptions, percentMode }: StackingRadioProps) => (
+  <RadioGroup
+    legend="Stacking?"
+    onChange={(e) => {
+      setChartOptions({
+        ...chartOptions,
+        plotOptions: { series: { stacking: e === '' ? undefined : e } },
+      });
+    }}
+    value={chartOptions.plotOptions!.series!.stacking ?? ''}
+  >
+    <Radio value={''}>Nei</Radio>
+    <Radio value={percentMode ? 'percent' : 'normal'}>Ja</Radio>
+  </RadioGroup>
+);
+
+const PercentModeSwitch = ({
+  chartOptions,
+  setChartOptions,
+  percentMode,
+  setPercentMode,
+}: PercentModeProps) => (
+  <Switch
+    checked={percentMode}
+    onChange={(e) => {
+      setPercentMode(e.target.checked);
+      setChartOptions({
+        ...chartOptions,
+        yAxis: { labels: { format: e.target.checked ? '{value}%' : '{value}' } },
+        tooltip: {
+          pointFormat: e.target.checked ? '{series.name}: {point.y}%' : '{series.name}: {point.y}',
+        },
+        plotOptions: {
+          series: {
+            stacking:
+              chartOptions.plotOptions!.series!.stacking != undefined
+                ? e.target.checked
+                  ? 'percent'
+                  : 'normal'
+                : undefined,
+          },
+        },
+      });
+    }}
+  >
+    Prosentmodus
+  </Switch>
+);
 
 export default ChartMenu;
