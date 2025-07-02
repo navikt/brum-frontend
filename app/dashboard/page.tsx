7333235d29d7e6603f1d75ff62afd23935102027
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-02 15:41:44 +0200

    redoing styling on dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Graph from '@/common/components/graph';
-import { PageBlock } from '@navikt/ds-react/Page';
+import { BodyShort, Heading, VStack } from '@navikt/ds-react';
+import { Page } from '@navikt/ds-react/Page';
 import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
@@ -18,12 +19,18 @@ export default function Dashboard() {
   }, [setData]);
   console.log(data);
   return (
-    <PageBlock>
-      <h1>Dashboard</h1>
-      <p>Velkommen til Brum Dashboard!</p>
-      <p>Her kan du se statistikk og annen informasjon.</p>
-
-      <Graph />
-    </PageBlock>
+    <Page>
+      <Page.Block as="main" width="2xl" gutters>
+        <VStack margin="4" gap="4" align="center">
+          <Heading level="1" size="xlarge">
+            Dashboard
+          </Heading>
+          <BodyShort>Velkommen til Brum Dashboard!</BodyShort>
+          <BodyShort>Her kan du se statistikk og annen informasjon.</BodyShort>
+          Analyse av tiltak
+          <Graph />
+        </VStack>
+      </Page.Block>
+    </Page>
   );
 }
