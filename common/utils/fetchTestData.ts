7b77c238405164b3fb85148f6f5321d9885e2862
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-02 12:23:26 +0200

    changed api

--- common/utils/fetchTestData.ts
+++ common/utils/fetchTestData.ts
@@ -4,7 +4,7 @@ import { UpdateSeriesProps } from '../types/propTypes';
 
 export function useFetchTestData(setData: Dispatch<SetStateAction<Object[]>>) {
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/testData`)
+    fetch(`${process.env.BRUM_API_URL}/testData`)
       .then((res) => {
         if (!res.ok) throw new Error('Failed to fetch data');
         return res.json();
