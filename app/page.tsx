090b1ce70e19e08b97cab74f481b8e0a7c1068bb
Nahom Berhane <nahom.berhane@nav.no>
2025-07-02 10:56:06 +0200

    refactor main page no need for auth check

--- app/page.tsx
+++ app/page.tsx
@@ -11,69 +11,9 @@ const Home = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const route = useRouter();
 
-  useEffect(() => {
-    const checkAuthStatus = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch('/oauth2/session', { credentials: 'include' });
-        if (!response.ok) throw new Error('Network response was not ok');
-        const authData = await response.json();
-        if (authData.session && authData.session.active) {
-          setIsAuthenticated(true);
-        } else {
-          setIsAuthenticated(false);
-        }
-      } catch (error) {
-        console.error('Error checking authentication:', error);
-        setError('Kunne ikke sjekke autentisering');
-        setIsAuthenticated(false);
-      } finally {
-        setLoading(false);
-      }
-    };
-    checkAuthStatus();
-  }, []);
-
-  const handleLoginClick = () => {
-    window.location.href = '/oauth2/login';
-  };
-
   const handleGoToDashboardClick = () => {
     route.push('/dashboard');
   };
-
-  if (error) {
-    return (
-      <main style={mainStyles}>
-        <div
-          style={{
-            minWidth: 340,
-            maxWidth: 400,
-            width: '100%',
-            margin: '0 auto',
-            background: '#fff',
-            borderRadius: 8,
-            boxShadow: '0 4px 24px #0001',
-            padding: '2rem',
-          }}
-        >
-          <ErrorSummary heading="Feil ved" size="medium">
-            <ErrorSummaryItem href="#1">Inloigging feilet. Vennligst prøv igjen.</ErrorSummaryItem>
-            <ErrorSummaryItem>ERROR: {error}</ErrorSummaryItem>
-          </ErrorSummary>
-        </div>
-      </main>
-    );
-  }
-
-  if (loading) {
-    return (
-      <main style={mainStyles}>
-        <Loader size="xlarge" title="Laster..." />
-        <p>Sjekker påloggingsstatus...</p>
-      </main>
-    );
-  }
 
   return (
     <main style={mainStyles}>
@@ -82,15 +22,9 @@ const Home = () => {
       </Heading>
       <p style={{ textAlign: 'center', marginBottom: '2rem', color: '#444' }}>Analyse av tiltak</p>
       <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '2rem' }}>
-        {isAuthenticated ? (
           <Button variant="primary" onClick={handleGoToDashboardClick}>
             Gå til Dashboard
           </Button>
-        ) : (
-          <Button variant="primary" onClick={handleLoginClick} style={{ width: '200px' }}>
-            Logg inn
-          </Button>
-        )}
       </div>
       <Spacer />
     </main>
