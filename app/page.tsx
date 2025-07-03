e3ea1f7ee189bcba9d4d74646c589bdc24022b86
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-03 09:53:10 +0200

    trying new theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,36 +2,40 @@
 
 import { BarChartIcon } from '@navikt/aksel-icons';
 import { Box, Heading, LinkCard, Page, VStack } from '@navikt/ds-react';
+import '@navikt/ds-css/darkside';
+import { Theme } from '@navikt/ds-react/Theme';
 
 const Home = () => {
   return (
-    <Page>
-      <Page.Block as="main" width="2xl" gutters>
-        <VStack margin="4" gap="4" align="center">
-          <Heading level="1" size="xlarge">
-            Velkommen til Brum
-          </Heading>
-          <Heading level="2" size="small">
-            Analyse av tiltak
-          </Heading>
-          <LinkCard data-color="Lightblue">
-            <Box
-              asChild
-              borderRadius="12"
-              padding="space-8"
-              style={{ backgroundColor: 'GrayAlpha' }}
-            >
-              <LinkCard.Icon>
-                <BarChartIcon fontSize="2rem" />
-              </LinkCard.Icon>
-            </Box>
-            <LinkCard.Title>
-              <LinkCard.Anchor href="/dashboard">Gå til dashboard</LinkCard.Anchor>
-            </LinkCard.Title>
-          </LinkCard>
-        </VStack>
-      </Page.Block>
-    </Page>
+    <Theme theme="light">
+      <Page>
+        <Page.Block as="main" width="2xl" gutters>
+          <VStack margin="4" gap="4" align="center">
+            <Heading level="1" size="xlarge">
+              Velkommen til Brum
+            </Heading>
+            <Heading level="2" size="small">
+              Analyse av tiltak
+            </Heading>
+            <LinkCard data-color="Lightblue">
+              <Box
+                asChild
+                borderRadius="12"
+                padding="space-8"
+                style={{ backgroundColor: 'GrayAlpha' }}
+              >
+                <LinkCard.Icon>
+                  <BarChartIcon fontSize="2rem" />
+                </LinkCard.Icon>
+              </Box>
+              <LinkCard.Title>
+                <LinkCard.Anchor href="/dashboard">Gå til dashboard</LinkCard.Anchor>
+              </LinkCard.Title>
+            </LinkCard>
+          </VStack>
+        </Page.Block>
+      </Page>
+    </Theme>
   );
 };
 
