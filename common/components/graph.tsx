de6f7b5c58c3f77eefb5154a2561be3c707b6a03
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-03 10:26:43 +0200

    loader for graph ++

--- common/components/graph.tsx
+++ common/components/graph.tsx
@@ -6,7 +6,7 @@ import ChartMenu from './chartmenu';
 import TableX from './tablex';
 import { Chart, HighchartsOptionsType } from '@highcharts/react';
 import { updateGraphSeries, useFetchTestData } from '@/common/utils/fetchTestData';
-import { HStack, Skeleton } from '@navikt/ds-react';
+import { HStack, Loader, Skeleton } from '@navikt/ds-react';
 
 const Graph = () => {
   const [data, setData] = useState<Object[]>([]);
@@ -26,14 +26,7 @@ const Graph = () => {
   return (
     <div>
       {loading ? (
-        <HStack gap="2">
-          <Skeleton variant="rectangle" width="8%" height="30%" />
-          <Skeleton variant="rectangle" width="5%" height="20%" />
-          <Skeleton variant="rectangle" width="9%" height="40%" />
-          <Skeleton variant="rectangle" width="6%" height="10%" />
-          <Skeleton variant="rectangle" width="8%" height="50%" />
-          <Skeleton variant="rectangle" width="7%" height="60%" />
-        </HStack>
+        <Loader size="xlarge" />
       ) : (
         <>
           <Chart options={chartOptions}>
