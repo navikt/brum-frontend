c993bdc76b3bb466d198410bddb335e58129bffd
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-04 10:03:24 +0200

    using local storage to store theme consistently

--- common/UI/themeProvider.tsx
+++ common/UI/themeProvider.tsx
@@ -5,8 +5,12 @@ import { Theme } from '@navikt/ds-react';
 import NavBar from '../components/NavBar';
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light');
-
+  const [theme, setTheme] = useState<'light' | 'dark'>(() => {
+    if (typeof window !== 'undefined') {
+      return (localStorage.getItem('theme') as 'light' | 'dark') || 'light';
+    }
+    return 'light';
+  });
   return (
     <Theme theme={theme}>
       <NavBar setTheme={setTheme} theme={theme} />
