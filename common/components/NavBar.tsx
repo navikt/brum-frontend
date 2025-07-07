df59868f8b331f0d75cc8958ba273604c98189a9
Nahom Berhane <nahom.berhane@nav.no>
2025-07-07 12:11:21 +0200

    refactor: improve user menu rendering logic in NavBar component

--- common/components/NavBar.tsx
+++ common/components/NavBar.tsx
@@ -5,21 +5,31 @@ import { UserMenu } from './UserMenu';
 import { BarChartIcon, ChevronDownIcon, PersonIcon } from '@navikt/aksel-icons';
 import { ThemeButton } from './ThemeButton';
 import { ActionMenu, Button, InternalHeader, Spacer, Theme } from '@navikt/ds-react';
-import { ActionMenuTrigger, ActionMenuContent, ActionMenuGroup, ActionMenuItem } from '@navikt/ds-react/ActionMenu';
+import {
+  ActionMenuTrigger,
+  ActionMenuContent,
+  ActionMenuGroup,
+  ActionMenuItem,
+} from '@navikt/ds-react/ActionMenu';
 import { UserInfo } from '../types/userInfoTypes';
 
-
 export default function NavBar() {
-  const user = useUser()
-
+  const user = useUser();
   return (
     <InternalHeader className="px-4" data-color="Blue-100">
       <InternalHeader.Title href="/">
         <BarChartIcon title="a11y-title" fontSize="1.5rem" />
         Brum
       </InternalHeader.Title>
-           {user && <UserMenu user={user} />}
-      <Spacer />
+
+      <Spacer /> 
+           {user ? (
+        <UserMenu user={user} />
+      ) : (
+        <Button as="a" href="/oauth2/login" variant="secondary-neutral">
+          Logg inn
+        </Button>
+      )}
       <ThemeButton />
     </InternalHeader>
   );
