9925b53e6dae68d33cf736d213a54aa3ab168ad5
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-07 09:48:41 +0200

    dynamically generating table content

---
+++ common/components/DataTable.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { Table } from '@navikt/ds-react';
+
+/*
+interface ScopedSortState extends SortState {
+  orderBy: keyof (typeof data)[0];
+} */
+
+const DataTable = ({ data }: { data: Record<string, any>[] }) => {
+  if (!data || data.length === 0) {
+    return <div>Ingen data</div>;
+  }
+  /*
+  const [sort, setSort] = useState<ScopedSortState | undefined>();
+
+  const handleSort = (sortKey: ScopedSortState['orderBy']) => {
+    setSort(
+      sort && sortKey === sort.orderBy && sort.direction === 'descending'
+        ? undefined
+        : {
+            orderBy: sortKey,
+            direction:
+              sort && sortKey === sort.orderBy && sort.direction === 'ascending'
+                ? 'descending'
+                : 'ascending',
+          },
+    );
+  };
+  
+
+  function comparator<T>(a: T, b: T, orderBy: keyof T): number {
+    if (b[orderBy] == null || b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const sortedData = data.slice().sort((a, b) => {
+    if (sort) {
+      return sort.direction === 'ascending'
+        ? comparator(b, a, sort.orderBy)
+        : comparator(a, b, sort.orderBy);
+    }
+    return 1;
+  });
+  */
+
+  const columns = Object.keys(data[0]);
+
+  return (
+    <Table zebraStripes>
+      <Table.Header>
+        <Table.Row>
+          {columns.map((col) => (
+            <Table.ColumnHeader key={col}>{col}</Table.ColumnHeader>
+          ))}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {data.map((row, i) => (
+          <Table.Row key={i}>
+            {columns.map((col) => (
+              <Table.DataCell key={col}>{row[col]}</Table.DataCell>
+            ))}
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  );
+};
+
+export default DataTable;
