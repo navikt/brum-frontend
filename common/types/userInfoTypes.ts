8bcc1f2723cd5c510901928ee1d40ebb70f65af0
Nahom Berhane <nahom.berhane@nav.no>
2025-07-07 09:39:42 +0200

    Refactor UserInfo interface to remove optional properties and enforce required NAVident field

--- common/types/userInfoTypes.ts
+++ common/types/userInfoTypes.ts
@@ -2,12 +2,7 @@ interface UserInfo {
   oid: string;
   username: string;
   groups: string[];
-  azp?: string;
-  azp_name?: string;
-  NAVident?: string;
-  roles?: string[];
-  scp?: string[];
-  idtyp?: string;
+  NAVident: string;
 }
 
 export type { UserInfo };
