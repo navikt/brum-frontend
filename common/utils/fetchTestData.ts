ac8549ae3d9dbbc1889ee5cc33e742803c2965fb
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-08 12:37:36 +0200

    ensuring update isn't performed with stale options

--- common/utils/fetchTestData.ts
+++ common/utils/fetchTestData.ts
@@ -18,30 +18,32 @@ export function useFetchTestData(
   }, [setData, dataParams]);
 }
 
-export function updateGraphSeries({
-  data,
-  chartOptions,
-  setChartOptions,
-  setLoading,
-  ref,
-}: UpdateSeriesProps) {
+export function updateGraphSeries({ data, setChartOptions, setLoading, ref }: UpdateSeriesProps) {
   if (!data || data.length === 0) {
     return;
   }
 
-  setChartOptions((prev: HighchartsOptionsType) => ({
-    ...prev,
+  const newOptions: HighchartsOptionsType = {
     series: data.map((d: Object) => ({
       type: 'column',
       data: Object.values(d).slice(1),
       name: String(Object.values(d)[0]),
     })),
     xAxis: [{ categories: Object.keys(data[0]).slice(1) }],
+  };
+
+  setChartOptions((prev: HighchartsOptionsType) => ({
+    ...prev,
+    ...newOptions,
   }));
 
-  // explictly updating options of the chart, so series update correctly
+  // explicitly updating the chart, because otherwise old series are left over when # old series > # new series
   if (ref.current?.chart) {
-    ref.current.chart.update(chartOptions, true, true); // (options,redraw,oneToOne). oneToOne=true ensures old series don't stay in the chart when # old series > # new series
+    ref.current.chart.update(
+      { ...ref.current.chart.options, ...newOptions },
+      true,
+      true, // oneToOne: ensures old series are removed from the graph
+    );
   }
 
   setLoading(false);
