300b5acbc8e94c04d54527039a4fc6715cbc321f
Nahom Berhane <nahom.berhane@nav.no>
2025-07-14 14:54:42 +0200

    new layout for dashboard and new mock data implmented

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import DataMenu from '@/common/components/DataMenu';
 import { BrumData } from '@/common/types/brumData';
 import { DataOptionsProps } from '@/common/types/propTypes';
 import useFetchUkeAntall from '@/common/utils/fetchUkeAntall';
-import { Heading, VStack } from '@navikt/ds-react';
+import { Heading, HGrid, HStack, VStack } from '@navikt/ds-react';
 import { Page } from '@navikt/ds-react/Page';
 import { useState } from 'react';
 
@@ -15,18 +15,29 @@ export default function Dashboard() {
 
   useFetchUkeAntall({ setData, dataParams });
 
-  return (
-    <Page>
-      <Page.Block width="2xl" as="main">
-        <VStack gap="3" margin="2" align="center">
-          <Heading level="1" size="xlarge">
-            Dashboard
-          </Heading>
-        </VStack>
-        <BrumChart data={data} />
-        <DataMenu dataParams={dataParams} setDataParams={setDataParams} />
+return (
+  <Page>
+    <Page.Block width="2xl" as="main">
+      <VStack gap="8">
+        <Heading level="1" size="xlarge" align="center">
+          Dashboard
+        </Heading>
+
+      
+        <HStack gap="8" align="start">
+          <div style={{ minWidth: '220px' }}>
+            <DataMenu dataParams={dataParams} setDataParams={setDataParams} />
+          </div>
+
+          <div style={{ flex: 1 }}>
+            <BrumChart data={data} />
+          </div>
+        </HStack>
+
         <BrumTable data={data!} />
-      </Page.Block>
-    </Page>
-  );
+      </VStack>
+    </Page.Block>
+  </Page>
+);
+
 }
--- app/test-error/page.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-dynamic";
-
-export default async function TestErrorPage() {
-  throw new Error("Simulated server error");
-
-  return (
-    <div>This won’t render.</div>
-  );
-}
