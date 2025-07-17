0a6bf75f45cfc10d8d6ce7be351914efb7907379
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-17 17:16:41 +0200

    table is sortable

--- components/data/BrumTable.tsx
+++ components/data/BrumTable.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { FunnelIcon, TableIcon } from '@navikt/aksel-icons';
-import { Page, Skeleton, Table, Tabs, VStack } from '@navikt/ds-react';
+import { Page, Skeleton, SortState, Table, Tabs, VStack } from '@navikt/ds-react';
 import { BrumTableProps } from '@/lib/types/propTypes';
 import { BrumData } from '@/lib/types/brumData';
+import { useState } from 'react';
 
 const BrumTable = ({ data, filteredData, filterTabRef }: BrumTableProps) => {
   if (!data) {
@@ -39,26 +40,91 @@ const BrumTable = ({ data, filteredData, filterTabRef }: BrumTableProps) => {
 
 export default BrumTable;
 
+interface ScopedSortState extends SortState {
+  orderBy: keyof BrumData['data'][number];
+}
+
 function DataTable({ data }: { data: BrumData }) {
+  const [sort, setSort] = useState<ScopedSortState | undefined>();
+
+  const handleSort = (sortKey: ScopedSortState['orderBy']) => {
+    setSort(
+      sort && sortKey === sort.orderBy && sort.direction === 'descending'
+        ? undefined
+        : {
+            orderBy: sortKey,
+            direction:
+              sort && sortKey === sort.orderBy && sort.direction === 'ascending'
+                ? 'descending'
+                : 'ascending',
+          },
+    );
+  };
+
+  function comparator<T>(a: T, b: T, orderBy: keyof T | string): number {
+    let aValue: any;
+    let bValue: any;
+
+    const orderByStr = String(orderBy);
+    if (orderByStr.startsWith('verdier.')) {
+      // Extract the index from "verdier.0", "verdier.1", etc.
+      const index = parseInt(orderByStr.split('.')[1]);
+      aValue = (a as any).verdier[index];
+      bValue = (b as any).verdier[index];
+    } else {
+      // Direct property access for avdeling and innsatsgruppe
+      aValue = a[orderBy as keyof T];
+      bValue = b[orderBy as keyof T];
+    }
+
+    if (bValue == null || bValue < aValue) {
+      return -1;
+    }
+    if (bValue > aValue) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const sortedData = data.data.sort((a, b) => {
+    if (sort) {
+      return sort.direction === 'ascending'
+        ? comparator(b, a, sort.orderBy)
+        : comparator(a, b, sort.orderBy);
+    }
+    return 1;
+  });
+
   return (
-    <Table size="small" zebraStripes>
+    <Table
+      sort={sort}
+      onSortChange={(sortKey) => handleSort(sortKey as ScopedSortState['orderBy'])}
+      size="small"
+      zebraStripes
+    >
       <Table.Header>
         <Table.Row>
-          <Table.ColumnHeader key={0} textSize="small">
+          <Table.ColumnHeader sortKey="avdeling" key={0} textSize="small" sortable>
             Avdeling
           </Table.ColumnHeader>
-          <Table.ColumnHeader key={1} textSize="small">
-            Innsattsgruppe
+          <Table.ColumnHeader sortKey="innsatsgruppe" key={1} textSize="small" sortable>
+            Innsatsgruppe
           </Table.ColumnHeader>
           {data.headers.map((h, i) => (
-            <Table.ColumnHeader align={'right'} key={i + 2} textSize="small">
+            <Table.ColumnHeader
+              align="right"
+              sortKey={`verdier.${i}`}
+              key={i + 2}
+              textSize="small"
+              sortable
+            >
               {h}
             </Table.ColumnHeader>
           ))}
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {data.data.map((row, i) => (
+        {sortedData.map((row, i) => (
           <Table.Row key={i}>
             <Table.DataCell key={0}>{row.avdeling}</Table.DataCell>
             <Table.DataCell key={1}>{row.innsatsgruppe}</Table.DataCell>
